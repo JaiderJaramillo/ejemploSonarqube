b0ceee41730606606b37c9a2205543ffa75cfd2a
JaiderJaramillo <60390850+JaiderJaramillo@users.noreply.github.com>
2025-03-31 08:14:01 -0500

    Update EjemploSonarApplication.java

--- src/main/java/com/example/ejemplosonar/EjemploSonarApplication.java
+++ src/main/java/com/example/ejemplosonar/EjemploSonarApplication.java
@@ -9,15 +9,4 @@ public class EjemploSonarApplication {
     public static void main(String[] args) {
         SpringApplication.run(EjemploSonarApplication.class, args);
     }
-
-
-
-	public void connectToDatabase() {
-		String username = System.getenv("DB_USERNAME");
-		String password = System.getenv("DB_PASSWORD");
-		System.out.println("Conectando a la base de datos con usuario: " + username + ""
-				+ " y contraseña: " + password);
-		// Lógica de conexión omitida
-	}
-	
 }
